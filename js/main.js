061ad6e9124e572f2ecab145f02052f5b1025a46
Marco Klein <marco.klein94@gmail.com>
2017-11-13 21:40:35 +0100

    centering canvas

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@
  */
 //noinspection JSCheckFunctionSignatures,JSCheckFunctionSignatures,JSCheckFunctionSignatures
 var game = new Phaser.Game(400, 400, Phaser.AUTO, document.getElementById('game'));
-//game.camera.width = 400;
-//game.camera.height = 400;
+game.stage.scale.pageAlignHorizontally = true;
+game.stage.scale.pageAlignVertically = true;
+game.stage.scale.refresh();
 game.state.add('Game',Game);
 game.state.start('Game');
